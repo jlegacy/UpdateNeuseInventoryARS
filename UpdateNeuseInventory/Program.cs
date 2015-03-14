0cc1ff7607f2ff9e275a9227e28746a34165c4b2
jlegacy <jlegacy@suddenlink.net>
2015-03-14 11:19:39 -0400

    Commit Changes

--- UpdateNeuseInventory/Program.cs
+++ UpdateNeuseInventory/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using LINQtoCSV;
 using UpdateNeuseInventory.Properties;
 
@@ -26,11 +27,11 @@ namespace UpdateNeuseInventory
             double count2 = 0;
             double count3 = 0;
             double notUsed = 0;
-            double notUsedQty = 0;
+            var searchCharacters = new StringBuilder();
 
-            String LeadingZeroItem = null;
+            //test
 
-            try
+        /*    try
             {
                 var inputFileDescription = new CsvFileDescription
                 {
@@ -44,7 +45,7 @@ namespace UpdateNeuseInventory
                     cc.Read<SectionCsv>(Settings.Default.tempFileName, inputFileDescription);
 
                 IEnumerable<SectionCsv> sectionsByName =
-                    from p in sections 
+                    from p in sections
                     where p.qty > 0
                     select p;
 
@@ -64,13 +65,13 @@ namespace UpdateNeuseInventory
                         continue;
                     }
 
-                    string searchCharacters = (item.upc.TrimStart().TrimStart('0'));
+                    searchCharacters.Clear();
+                    searchCharacters.Append(item.upc.TrimStart().TrimStart('0'));
 
                     var cs = new ProductsDataContext();
                     IQueryable<product> q =
                         from a in cs.GetTable<product>()
-                        where (a.pID.Contains(searchCharacters))
-                              && item.qty > 0
+                        where (a.pID.Contains(searchCharacters.ToString()))
                         select a;
 
                     foreach (product a in q)
@@ -89,17 +90,71 @@ namespace UpdateNeuseInventory
                             Console.Write(e.ToString());
                         }
                     }
-                }
+                } 
+
+               
             }
             catch (Exception e)
             {
                 Console.Write(e.Message);
+            } */
+
+            ZeroInventory();
+        }
+
+        private static void ZeroInventory()
+        {
+            ProductsDataContext cs;
+            int count = 0;
+            cs = new ProductsDataContext();
+            IQueryable<product> z =
+                from a in cs.GetTable<product>()
+                select a;
+
+            foreach (product a in z)
+            {
+                buildData2(a);
+                try
+                {
+                    cs.SubmitChanges();
+                    count = count + 1;
+                    Console.SetCursorPosition(0, 5); //move cursor
+                    Console.Write("Final Update Record: " + count);
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.ToString());
+                }
             }
+         
         }
 
         private static void buildData(product myProduct, SectionCsv item)
         {
-            myProduct.pInStock = Convert.ToInt32(item.qty);
+            myProduct.pCustom1 = Convert.ToString(item.qty);
+            myProduct.pCustom2 = "p";
+        }
+
+        private static void buildData2(product myProduct)
+        {
+            if ((myProduct.pCustom2 == null))
+            {
+                myProduct.pCustom2 = "";
+                myProduct.pCustom1 = "0";
+            }
+            else
+            {
+                if ((myProduct.pCustom2.CompareTo("p") == 0))
+                {
+                    myProduct.pCustom2 = "";
+                }
+                else
+                {
+                    myProduct.pCustom1 = "0";
+                    myProduct.pCustom2 = "";  
+                }
+                
+            }
         }
 
         public class Result
